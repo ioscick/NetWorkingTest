ca8d178c159508822bca8b3f96e22035641aa51f
shenliping <shenlp@gegejia.com>
2018-08-15 18:12:29 +0800

    Initial Commit

---
+++ 测试网络图片/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  测试网络图片
+//
+//  Created by 沈立平 on 2018/8/15.
+//  Copyright © 2018年 沈立平. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 测试网络图片/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  测试网络图片
+//
+//  Created by 沈立平 on 2018/8/15.
+//  Copyright © 2018年 沈立平. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
